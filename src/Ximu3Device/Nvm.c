fd000836ca91b28766ed8b5cc765853f78f786f6
x-io Technologies <social@x-io.co.uk>
2026-03-07 10:47:33 +0000

    Avoid magic numbers in NVM addresses

--- src/Ximu3Device/Nvm.c
+++ src/Ximu3Device/Nvm.c
@@ -7,6 +7,7 @@
 //------------------------------------------------------------------------------
 // Includes
 
+#include "Config.h"
 #include "Context.h"
 #include "Eeprom/Eeprom.h"
 #include "I2C/I2C1.h"
@@ -18,29 +19,39 @@
 #include "Nvm.h"
 
 //------------------------------------------------------------------------------
+// Definitions
+
+/**
+ * @brief Quotient size.
+ */
+#define QUOTIENT_SIZE (EEPROM_SIZE / 4)
+
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
 // Variables
 
 const Nvm nvmMain = {.i2c = &i2cBB1, .address = 0};
 const Nvm nvmA = {.i2c = &i2c3, .address = 0};
-const Nvm nvmB = {.i2c = &i2c3, .address = 1024};
-const Nvm nvmC = {.i2c = &i2c3, .address = 2048};
-const Nvm nvmD = {.i2c = &i2c3, .address = 3072};
+const Nvm nvmB = {.i2c = &i2c3, .address = nvmA.address + QUOTIENT_SIZE};
+const Nvm nvmC = {.i2c = &i2c3, .address = nvmB.address + QUOTIENT_SIZE};
+const Nvm nvmD = {.i2c = &i2c3, .address = nvmC.address + QUOTIENT_SIZE};
 const Nvm nvmE = {.i2c = &i2c2, .address = 0};
-const Nvm nvmF = {.i2c = &i2c2, .address = 1024};
-const Nvm nvmG = {.i2c = &i2c2, .address = 2048};
-const Nvm nvmH = {.i2c = &i2c2, .address = 3072};
+const Nvm nvmF = {.i2c = &i2c2, .address = nvmE.address + QUOTIENT_SIZE};
+const Nvm nvmG = {.i2c = &i2c2, .address = nvmF.address + QUOTIENT_SIZE};
+const Nvm nvmH = {.i2c = &i2c2, .address = nvmG.address + QUOTIENT_SIZE};
 const Nvm nvmI = {.i2c = &i2c5, .address = 0};
-const Nvm nvmJ = {.i2c = &i2c5, .address = 1024};
-const Nvm nvmK = {.i2c = &i2c5, .address = 2048};
-const Nvm nvmL = {.i2c = &i2c5, .address = 3072};
+const Nvm nvmJ = {.i2c = &i2c5, .address = nvmI.address + QUOTIENT_SIZE};
+const Nvm nvmK = {.i2c = &i2c5, .address = nvmJ.address + QUOTIENT_SIZE};
+const Nvm nvmL = {.i2c = &i2c5, .address = nvmK.address + QUOTIENT_SIZE};
 const Nvm nvmM = {.i2c = &i2c1, .address = 0};
-const Nvm nvmN = {.i2c = &i2c1, .address = 1024};
-const Nvm nvmO = {.i2c = &i2c1, .address = 2048};
-const Nvm nvmP = {.i2c = &i2c1, .address = 3072};
+const Nvm nvmN = {.i2c = &i2c1, .address = nvmM.address + QUOTIENT_SIZE};
+const Nvm nvmO = {.i2c = &i2c1, .address = nvmN.address + QUOTIENT_SIZE};
+const Nvm nvmP = {.i2c = &i2c1, .address = nvmO.address + QUOTIENT_SIZE};
 const Nvm nvmQ = {.i2c = &i2c4, .address = 0};
-const Nvm nvmR = {.i2c = &i2c4, .address = 1024};
-const Nvm nvmS = {.i2c = &i2c4, .address = 2048};
-const Nvm nvmT = {.i2c = &i2c4, .address = 3072};
+const Nvm nvmR = {.i2c = &i2c4, .address = nvmQ.address + QUOTIENT_SIZE};
+const Nvm nvmS = {.i2c = &i2c4, .address = nvmR.address + QUOTIENT_SIZE};
+const Nvm nvmT = {.i2c = &i2c4, .address = nvmS.address + QUOTIENT_SIZE};
 
 //------------------------------------------------------------------------------
 // Functions
